6a2e4bfb98edf44000c6ed93c098256547de16a4
Anurag Singh <44972571+AnuragSingh-01@users.noreply.github.com>
2019-08-25 03:09:11 +0530

    Update SegmenetTree.java

--- SegmenetTree.java
+++ SegmenetTree.java
@@ -20,8 +20,19 @@ public class SegmenetTree
    public static void main(String args[])
    {
       int arr[]=new int[]{1,2,3,4,5,6,7,8,9,10};
-      int N=arr.length;
-      int sgTree=new int [2*N];
+     /*
+       int N=arr.length;
+       int sgTree=new int [2*N];
+       if you are writing code in C++ Above will be fine 
+       
+       other wise in Java -----
+       if the size of given array is power of 2 then segment tree size will be (size of array)*2-1 
+                                                 otherwise (next power of 2 near to that number)*2-1.
+      */
+      int height = (int)(Math.log(n)/Math.log(2)) + 1;
+      int tree_nodes = (int) Math.pow(2, height + 1);
+      
+      int sgTree=new int [tree_nodes];
       
       buildSG(arr,sgTree,0,N-1,1);
       
